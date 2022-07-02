26c66bedbf58c28991275c6bcda52a68f33c197d
Gabriel Del Sent Hellmann <100445201+gabriel-hellmann@users.noreply.github.com>
2022-07-02 11:51:41 -0300

    Update ex002.py

--- POO/ex002.py
+++ POO/ex002.py
@@ -1,9 +1,3 @@
-"""Classe Quadrado: Crie uma classe que modele um quadrado:
-Atributos: Tamanho do lado
-Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
-"""
-
-
 class Quadrado:
     def __init__(self, tamanho_do_lado):
         self.tamanho = tamanho_do_lado
